46c17d52aa32f3cb71324d0352958cbdc12078cb
Louie Camacho <louiecamacho@gmail.com>
2026-04-25 08:59:41 -0500

    add columns function

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,10 +1,24 @@
 
+import { useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
+import type { Column } from "../types";
 
 export default function KanbanBoard() {
+  const [columns, setColumns] = useState<Column[]>([]);
+  console.log(columns);
+  
 
   function createNewColumn() {
-    
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+
+    setColumns([...columns, columnToAdd]);
+  }
+
+  function generateId() {
+    return Math.floor(Math.random() * 10001);
   }
 
   return (
@@ -18,7 +32,10 @@ export default function KanbanBoard() {
     overflow-y-hidden
     px-[40px]
     ">
-      <div className="m-auto">
+      <div className="m-auto flex gap-4">
+        <div className="flex gap-4">{columns.map((col)=>(
+          <div key={col.id}>{col.title}</div>
+        ))}</div>
         <button onClick={()=>{
           createNewColumn();
         }}
